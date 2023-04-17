e2eef73cc1f262d0f8a9a3f9b9762502123f6a58
Matt Nappo <mattnappo@gmail.com>
2023-04-17 11:30:36 -0400

    Wrote skeleton code for ACO

---
+++ include/aco.hpp
@@ -0,0 +1,19 @@
+// Compute the edge attractiveness matrix given the graph, tau, eta, a, and b.
+// Store the output in `float *A`
+void edge_attractiveness(float *A, float *adjacency_matrix, float *tau, float *eta,
+        float a, float b)
+{
+
+}
+
+// Run a single ant, which will update tau
+void run_ant(float *adjacency_matrix, float *tau, float *A, float x_best, float y_best)
+{
+
+}
+
+// Run the ant colony optimization algorithm on a graph.
+void run_aco(float *adjacency_matrix, int m, int k_max, float a, float b, float p)
+{
+
+}
